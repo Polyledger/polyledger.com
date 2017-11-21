c4623ede08683b91f7a263d211ea85c9909d3f59
Matt Rosendin <matt.rosendin@gmail.com>
2017-11-20 20:17:32 -0800

    add variety of gulp tasks

--- README.md
+++ README.md
@@ -2,4 +2,32 @@
 
 > The official Polyledger website, hosted on GitHub pages.
 
-<kbd><img src="https://raw.githubusercontent.com/polyledger/polyledger.com/master/screenshot.png"></kbd>
+<kbd><img src="https://github.com/polyledger/polyledger.com/blob/master/screenshot.png"></kbd>
+
+## Development
+
+Clone from GitHub
+
+```
+$ git clone git@github.com:polyledger/polyledger.com.git && cd polyledger.com
+```
+
+Install the NPM modules
+
+```
+$ npm i
+```
+
+Run [gulp](https://gulpjs.com/)
+
+```
+$ gulp
+```
+
+## Deployment
+
+Use subtree push to send `/dist` to the `gh-pages` branch.
+
+```
+git subtree push --prefix dist origin gh-pages
+```
---
+++ gulpfile.babel.js
@@ -0,0 +1,67 @@
+'use-strict'
+
+let pump       = require('pump')
+let del        = require('del')
+let gulp       = require('gulp')
+let util       = require('gulp-util')
+let babel      = require('gulp-babel')
+let concat     = require('gulp-concat')
+let minifyHTML = require('gulp-htmlmin')
+let minifyCSS  = require('gulp-clean-css')
+let sourcemaps = require('gulp-sourcemaps')
+let cachebust  = require('gulp-cache-bust')
+let minifyJS   = require('gulp-uglify')
+let deploy     = require('gulp-gh-pages')
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', ['minify-html', 'minify-css', 'minify-js'], () => {
+  return gulp.src(['css/**/*.min.css', 'js/**/*.min.js'])
+    .pipe(cachebust({type: 'timestamp'}))
+    .pipe(deploy())
+})
+
+gulp.task('clean-js', () => {
+	return del(['js/bundle.min.js'])
+})
+ 
+gulp.task('clean-css', () => {
+	return del(['css/stylesheet.min.css'])
+})
+
+gulp.task('minify-html', () => {
+  return gulp.src('*.html')
+    .pipe(minifyHTML({collapseWhitespace: false, removeComments: true}))
+    .pipe(gulp.dest('.'))
+})
+
+gulp.task('minify-css', ['clean-css'], () => {
+  return gulp.src('css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(concat('stylesheet.min.css'))
+    .pipe(minifyCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('.'))
+})
+
+gulp.task('minify-js', ['clean-js'], () => {
+  return gulp.src(['js/**/*.js', '!js/vendor/**'])
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.min.js'))
+    .pipe(babel())
+    .pipe(minifyJS())
+    .on('error', (err) => { util.log(util.colors.red('[Error]'), err.toString())})
+    .pipe(gulp.dest('js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('.'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch('index.html', ['minify-html'])
+  gulp.watch(['css/**/*.css', '!css/stylesheet.min.css'], ['minify-css'])
+  gulp.watch(['js/**/*.js', '!js/bundle.min.css'], ['minify-js'])
+})
+
+gulp.task('default', ['watch'])

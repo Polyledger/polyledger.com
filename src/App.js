b0ab493d790cbe37d9230deb2efc0a0a169458d8
Matt Rosendin <matt.rosendin@gmail.com>
2018-02-18 15:12:07 -0800

    Install styling modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import "./assets/css/toolkit.css";
 
 class App extends Component {
 	render() {
@@ -14,6 +13,7 @@ class App extends Component {
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
+				<button class="btn btn-primary">Test</button>
 			</div>
 		);
 	}

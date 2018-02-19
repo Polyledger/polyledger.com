8ea3fd732cb2e9d5e0da4844dea875bd6384a7c3
Matt Rosendin <matt.rosendin@gmail.com>
2018-02-19 09:58:56 -0800

    Add nav links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,10 +7,42 @@ class Navbar extends Component {
 	render() {
 		return (
 			<div className="Navbar fixed-top">
-				<nav className="container navbar navbar-padded navbar-toggleable-sm">
-					<Link to="/" className="navbar-brand mr-auto">
-						<img src={logo} className="Navbar-logo" height="50" alt="logo" />
+				<nav className="container navbar navbar-padded navbar-expand-lg navbar-light">
+					<Link to="/" className="navbar-brand">
+						<img src={logo} height="50" alt="logo" />
 					</Link>
+
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-toggle="collapse"
+						data-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon" />
+					</button>
+
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav ml-auto">
+							<li className="nav-item active">
+								<a className="nav-link" href="">
+									Home
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="">
+									Methodology
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link disabled" href="">
+									Log In
+								</a>
+							</li>
+						</ul>
+					</div>
 				</nav>
 			</div>
 		);
